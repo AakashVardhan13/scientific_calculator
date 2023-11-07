7a62d1e94ee541312592a5a849f864918ac62eb4
AakashVardhan13 <chav13.2003@gmail.com>
2023-11-07 23:49:59 +0530

    after final commit

--- src/test/java/Calcultor_Test.java
+++ src/test/java/Calcultor_Test.java
@@ -12,65 +12,64 @@ public class Calcultor_Test{
 
     @Test
     public void test_add_positive(){
-        int x = 1;
-        int y = 2;
-        int expectedResult = 3;
+        int x = 2;
+        int y = 3;
+        int expectedResult = 5;
         Assert.assertEquals(expectedResult, calculator.add(x, y));
     }
 
     @Test
     public void test_add_negative(){
-        int x = 1;
-        int y = 2;
-
-        int expectedResult = 5;
+        int x = 5;
+        int y = 1;
+        int expectedResult = 2;
         Assert.assertNotEquals(expectedResult, calculator.add(x, y));
     }
 
     @Test
     public void test_sub_positive(){
-        int x = 5;
-        int y = 2;
-        int expectedResult = 3;
+        int x = 8;
+        int y = 3;
+        int expectedResult = 5;
         Assert.assertEquals(expectedResult, calculator.sub(x, y));
     }
 
     @Test
     public void test_sub_negative(){
-        int x = 5;
+        int x = 8;
         int y = 2;
-        int expectedResult = 0;
+        int expectedResult = 7;
         Assert.assertNotEquals(expectedResult, calculator.sub(x, y));
     }
 
     @Test
     public void test_mul_positive(){
-        int x = 5;
-        int y = 2;
-        int expectedResult = 10;
+        int x = 3;
+        int y = 3;
+        int expectedResult = 9;
         Assert.assertEquals(expectedResult, calculator.mul(x, y));
     }
 
     @Test
     public void test_mul_negative(){
-        int x = 5;
-        int y = 2;
-        int expectedResult = 5;
+        int x = 4;
+        int y = 7;
+        int expectedResult = 9;
         Assert.assertNotEquals(expectedResult, calculator.mul(x, y));
     }
 
     @Test
     public void test_div_positive(){
-        int x = 6;
+        int x = 3;
         int y = 2;
-        int expectedResult = 3;
+        int expectedResult = 1;
         Assert.assertEquals(expectedResult, calculator.div(x, y));
     }
     @Test
     public void test_div_negative(){
-        int x = 5;
+        int x = 6;
         int y = 2;
-        int expectedResult = 4;
+        int expectedResult = 5;
         Assert.assertNotEquals(expectedResult, calculator.div(x, y));
     }
 
