bbb64e5ea6c83c7d445f23955165dc2e44932086
AakashVardhan13 <chav13.2003@gmail.com>
2023-11-07 22:37:36 +0530

    final commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,94 @@
 package org.example;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+
+    // private static final Logger logger = LogManager.getLogger(Main.class);
+    public static int add(int x, int y){
+
+        return x + y;
+    }
+
+    public static int sub(int x, int y){
+
+        return x - y;
+    }
+
+    public static int mul(int x, int y){
+
+        return x * y;
+    }
+
+    public static int div(int x, int y){
+
+        return x / y;
+    }
+
+
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
+        int a1,a2,s1,s2,m1,m2,d1,d2;
+        boolean flag = true;
 
+        while (flag){
+
+            System.out.println("-------Welcome to Calculator------");
+            System.out.println("What do you want to do?");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Exit");
+
+            Scanner sc = new Scanner(System.in);
+            int choice = sc.nextInt();
+
+            switch(choice) {
+
+                case 1:
+                    System.out.println("Enter First Number: ");
+                    a1 = sc.nextInt();
+                    System.out.println("Enter Second Number: ");
+                    a2 = sc.nextInt();
+                    System.out.println("Result: " + add(a1, a2));
+                    break;
+
+                case 2:
+                    System.out.println("Enter First Number: ");
+                    s1 = sc.nextInt();
+                    System.out.println("Enter Second Number: ");
+                    s2 = sc.nextInt();
+                    System.out.println("Result: " + sub(s1, s2));
+                    break;
+
+                case 3:
+                    System.out.println("Enter First Number: ");
+                    m1 = sc.nextInt();
+                    System.out.println("Enter Second Number: ");
+                    m2 = sc.nextInt();
+                    System.out.println("Result: " + mul(m1, m2));
+                    break;
+
+                case 4:
+                    System.out.println("Enter First Number: ");
+                    d1 = sc.nextInt();
+                    System.out.println("Enter Second Number: ");
+                    d2 = sc.nextInt();
+                    System.out.println("Result: " + div(d1, d2));
+                    break;
+
+                case 5:
+                    System.out.println("Thanks you");
+                    flag = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid Input!");
+                    break;
+            }
+        }
     }
 }
--- src/test/java/Calcultor_Test.java
+++ src/test/java/Calcultor_Test.java
@@ -1,2 +1,77 @@
-public class Calcultor_Test {
+import org.example.Main;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Calcultor_Test{
+    private Main calculator;
+    @Before
+    public void setUp(){
+        calculator = new Main();
+    }
+
+    @Test
+    public void test_add_positive(){
+        int x = 1;
+        int y = 2;
+        int expectedResult = 3;
+        Assert.assertEquals(expectedResult, calculator.add(x, y));
+    }
+
+    @Test
+    public void test_add_negative(){
+        int x = 1;
+        int y = 2;
+
+        int expectedResult = 5;
+        Assert.assertNotEquals(expectedResult, calculator.add(x, y));
+    }
+
+    @Test
+    public void test_sub_positive(){
+        int x = 5;
+        int y = 2;
+        int expectedResult = 3;
+        Assert.assertEquals(expectedResult, calculator.sub(x, y));
+    }
+
+    @Test
+    public void test_sub_negative(){
+        int x = 5;
+        int y = 2;
+        int expectedResult = 0;
+        Assert.assertNotEquals(expectedResult, calculator.sub(x, y));
+    }
+
+    @Test
+    public void test_mul_positive(){
+        int x = 5;
+        int y = 2;
+        int expectedResult = 10;
+        Assert.assertEquals(expectedResult, calculator.mul(x, y));
+    }
+
+    @Test
+    public void test_mul_negative(){
+        int x = 5;
+        int y = 2;
+        int expectedResult = 5;
+        Assert.assertNotEquals(expectedResult, calculator.mul(x, y));
+    }
+
+    @Test
+    public void test_div_positive(){
+        int x = 6;
+        int y = 2;
+        int expectedResult = 3;
+        Assert.assertEquals(expectedResult, calculator.div(x, y));
+    }
+    @Test
+    public void test_div_negative(){
+        int x = 5;
+        int y = 2;
+        int expectedResult = 4;
+        Assert.assertNotEquals(expectedResult, calculator.div(x, y));
+    }
+
 }
